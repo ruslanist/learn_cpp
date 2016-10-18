ca85044199ce37ec2b4fef28858bdfd62b31ba1d
Ruslan <you@example.com>
2016-10-19 02:27:50 +0800

    New Metod and Konstr

--- ProdjektGit/main.cpp
+++ ProdjektGit/main.cpp
@@ -21,7 +21,40 @@ public:
     }
     
     void displayMessage2() {
-    cout << "Добро пожаловать GitHub2!" << endl;
+        cout << "Hello!" << endl;
+    }
+    
+    void git_match()
+    {
+        // Объявление переменных
+     int sum1=154, sum2=234, sum3=367, sum4=576, sum5=754, sum6=635;
+        
+        // Вычисление  переменных sum1, 2, 3. 4. 5. 6
+        int a= sum1 + sum4;
+        int b= sum2 * sum3;
+        int c= sum5 - sum6;
+        
+        // Вычисление Итоговой Суммы переменных a, b и c
+        int itogo= a+b+c;
+        
+        // Отоброжения равенства a, b, c
+        cout << a << b << c << endl; 
+        
+        // Отображение итоговой суммы переменных a, b и c
+        cout << itogo << endl;
+    }
+    
+    // Обьявление Конструктора GitHub
+    GitHub() 
+    {
+        int consruct1=25, construct2=50, construct3=75, construct4=100;
+        
+        int itog_constr;
+        
+        
+        itog_constr = construct2 + construct4;
+        
+        cout << itog_constr << endl;
     }
         
 }; // Конец Класса GitHub
@@ -35,6 +68,9 @@ int main(int argc, char** argv) {
     
     myGitHub.displayMessage(); // Вызов метода displayMessage() класса GitHub
     
+    myGitHub.displayMessage2(); // Вызов метода displayMessage2() класса GitHub
+
+    myGitHub.git_match(); // Вызов метода git_match() класса GitHub
 
     return 0;
 }
