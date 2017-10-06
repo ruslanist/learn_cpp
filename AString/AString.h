9f42b70f3c918c5ef68b77a8400a070f7613c05e
Nikolay Kinash <peroksid90@gmail.com>
2017-10-06 13:46:34 +0800

    new tasks

--- AString/AString.h
+++ AString/AString.h
@@ -35,8 +35,21 @@ class AString {
 		const static size_t npos = -1;
                 
 		AString(const char* input); // Конструктор
+
+		//Сделать реализацию для этого конструктора.
+		//Суть его работы в том, что он должен взять не всю строчку из input и сохранить в себя,
+		//а только len символов из input сохранить в себя
+		AString(const char* input, size_t len);
+
 		~AString(); // Диструктор
                 
+		//Реализовать эту функцию.
+		//Суть ее работы в том, что она возвращает индекс первого символа, не равного ch
+		//Например если есть строка "......word...."
+		//и на нее зовут find_first_not_of(.)  - т.е. искать первый символ, не равный точке
+		//то она вернет 6 индекс, на 6 индексе находится буква w
+		size_t find_first_not_of(char ch) const;
+
 		size_t find_first_of(char ch) const; // Функция
                 
 		size_t find_last_of(char ch) const; // Функция
