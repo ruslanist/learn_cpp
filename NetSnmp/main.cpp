031f83025df0bec11830b1f27754b42f5bec3d50
Ruslan Istomin <ruslanist@yandex.ru>
2018-01-29 20:10:42 +0800

    NetSnmString_Oid_Mod15

--- NetSnmp/main.cpp
+++ NetSnmp/main.cpp
@@ -30,6 +30,13 @@ int main(int argc, char** argv) {
         NetSnmp netSnmp("127.0.0.1", "public");
         cout << "Количество функций, которое предоставляет это оборудование: " << netSnmp.getInt("1.3.6.1.2.1.1.7.0") << endl;
    
+        
+        NetSnmp snmpAid("1.3.6.1.2.1.1.1.0", "public");
+        cout << "Определение вендора/модели/Имя устройства: " << snmpAid.getString("1.3.6.1.2.1.1.1.0") << endl;
+        
+        NetSnmp snmpAid("1.3.6.1.4.1.9.10.24.1.1.2.0", "public");
+        cout << "Число юзеров активных всего (SessionTotal): " << snmpAid.getString("1.3.6.1.4.1.9.10.24.1.1.2.0") << endl;
+        
    } // Конец Блока try
    
    catch(const SnmpError &snmpError) {
