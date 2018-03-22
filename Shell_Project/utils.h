ff98e940e689a64c4a5ea377f4945cdfba7da41e
Ruslan Istomin <ruslanist@yandex.ru>
2018-03-22 11:36:20 +0800

    Shell_Project_Mode1

---
+++ Shell_Project/utils.h
@@ -0,0 +1,25 @@
+
+
+/* 
+ * File:   utils.h
+ * Author: ruslan
+ *
+ * Created on 22 марта 2018 г., 9:43
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UTILS_H */
+
