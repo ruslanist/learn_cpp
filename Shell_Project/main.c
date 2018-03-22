96c85daf4f9852da4073b9a9ca4bc538bccf344a
Ruslan Istomin <ruslanist@yandex.ru>
2018-03-22 11:32:49 +0800

    Shell_Project

---
+++ Shell_Project/main.c
@@ -0,0 +1,22 @@
+#include<utils.h>
+
+int main(int argc, char * argv[]) {
+    
+#ifdef NEED_LOG
+
+    fprintf();
+    
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Shell_Project/utils.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   utils.c
+ * Author: ruslan
+ *
+ * Created on 22 марта 2018 г., 10:11
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
